43e4568ff5a96658828bc69905bc03d903077376
cinda87 <j.c.bietz@gmail.com>
2025-02-06 11:39:11 -0700

    Improved the UX

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function displayWorkout(response) {
-  let workoutElement = document.getElementById("workout");
+  let workoutElement = document.querySelector("#workout");
   workoutElement.innerHTML = "";
   new Typewriter(workoutElement, {
     strings: response.data.answer,
@@ -12,33 +12,24 @@ function displayWorkout(response) {
 function generateWorkout(event) {
   event.preventDefault();
 
-  let workoutTypeElement = document.getElementById("workout-type");
-  let fitnessLevelElement = document.getElementById("fitness-level");
-  let goalsElement = document.getElementById("goals");
-  let timeElement = document.getElementById("time");
-  let injuriesElement = document.getElementById("injuries");
+  let workoutTypeElement = document.querySelector("#workout-type");
+  let fitnessLevelElement = document.querySelector("#fitness-level");
+  let goalsElement = document.querySelector("#goals");
+  let timeElement = document.querySelector("#time");
+  let injuriesElement = document.querySelector("#injuries");
 
   let apiKey = "a050491735e3o6daf6dd43f3ab206bct";
-  let context = `You are an expert workout coach and specialize in creating short workouts with a warm-up and cool-down. Your task is to generate a workout in basic HTML with a maximum of 45 lines. Follow the user’s instructions carefully. For each exercise, provide a link that should open in a new tab when clicked, for example https://www.youtube.com/results?search_query=push+ups. At the end of the workout, sign it with SheCodes AI inside a <strong> element. Avoid using quotes and the word html at the beginning. Thank you.`;
+  let context = `You are an expert workout coach and specialize in creating short workouts with a warm-up and cool-down. Your task is to generate a workout in basic HTML. Follow the user’s instructions carefully. For each exercise, provide a link that should open in a new tab when clicked, for example https://www.youtube.com/results?search_query=push+ups. At the end of the workout, sign it with SheCodes AI inside a <strong> element. Avoid using quotes and the word html at the beginning. Thank you.`;
   let prompt = `User instructions: Generate a ${workoutTypeElement.value} workout for a ${fitnessLevelElement.value}, ${goalsElement.value} goal for ${timeElement.value} minutes. Please keep in mind any injuries or limitations such as ${injuriesElement.value} if blank ignore.`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  let workoutElement = document.getElementById("workout");
+  let workoutElement = document.querySelector("#workout");
   workoutElement.classList.remove("hidden");
-  workoutElement.innerHTML = `<h2 class= "generating">⏳ Generating a ${timeElement.value} minute ${fitnessLevelElement.value} ${workoutTypeElement.value} workout for your ${goalsElement.value} goals.</h2><h1 color="red">Disclaimer: If you engage in this workout program, you agree that you do so at your own risk, are voluntarily participating in these activities, and assume all risk of injury to yourself...</h1>`;
+  workoutElement.innerHTML = `<h3 class= "generating">⏳ Generating a ${timeElement.value} minute ${fitnessLevelElement.value} ${workoutTypeElement.value} workout for your ${goalsElement.value} goals.</h3><h2 color="red">Disclaimer: If you engage in this workout program, you agree that you do so at your own risk, are voluntarily participating in these activities, and assume all risk of injury to yourself...</h2>`;
 
   axios.get(apiURL).then(displayWorkout);
 }
 
-let workoutFormElement = document.getElementById("workout-generator-form");
+let workoutFormElement = document.querySelector("#workout-generator-form");
 workoutFormElement.addEventListener("submit", generateWorkout);
 
-
-
-
-let workoutElement = document.getElementById("workout");
-const downloadBtn = document.querySelector(".download-btn");
-
-downloadBtn.addEventListener("click", () => {
-  print(workoutElement.value);
-});
